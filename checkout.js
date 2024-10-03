44ee2ef828d8819421ecf520627c7698be196d01
kellychibz <86753044+kellychibz@users.noreply.github.com>
2024-10-03 09:48:30 +0200

    Add files via upload

---
+++ checkout.js
@@ -0,0 +1,24 @@
+console.log("In JS");
+document.getElementById('checkout-form').addEventListener('submit', handleCheckout);
+async function handleCheckout(event) {
+event.preventDefault();
+const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/xml");
+
+const raw = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<API3G>\r\n  <CompanyToken>B3F59BE7-0756-420E-BB88-1D98E7A6B040</CompanyToken>\r\n  <Request>createToken</Request>\r\n  <Transaction>\r\n    <PaymentAmount>50.00</PaymentAmount>\r\n    <PaymentCurrency>USD</PaymentCurrency>\r\n    <CompanyRef>49FKEOA</CompanyRef>\r\n    <RedirectURL>http://www.domain.com/payurl.php</RedirectURL>\r\n    <BackURL>http://www.domain.com/backurl.php </BackURL>\r\n    <CompanyRefUnique>0</CompanyRefUnique>\r\n    <PTL>5</PTL>\r\n  </Transaction>\r\n  <Services>\r\n    <Service>\r\n      <ServiceType>85325</ServiceType>\r\n      <ServiceDescription>Flight from Nairobi to Diani</ServiceDescription>\r\n      <ServiceDate>2013/12/20 19:00</ServiceDate>\r\n    </Service>\r\n  </Services>\r\n</API3G>";
+
+const requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
+
+try {
+  const response = await fetch("https://secure.3gdirectpay.com/API/v6/", requestOptions);
+  const result = await response.text();
+  console.log(result)
+} catch (error) {
+  console.error(error);
+};
+}
